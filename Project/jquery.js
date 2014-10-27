c6bfed0affd0f8deecf37cbf78e20f0c5e3564f7
Kat <kat.eigelbach@gmail.com>
2014-10-26 23:04:42 -0400

    new nav buttons

--- Project/jquery.js
+++ Project/jquery.js
@@ -1,14 +1,21 @@
 //Hide Navigation list and add menu link before
-$("#nav").addClass("js").before('<div id="menu">&#9776; <h2>Louisville Metro Parks</h2></div>');
+$("#nav").addClass("js").before('<div id="menu"><h2>&#9776; Louisville Metro Parks</h2></div>');
 
 //add click listener and toggle the visibility
 $("#menu").click(function() {
 	$("#nav").toggle();
 });
 
-//hide nav when viewport increases
+//hide dropped down nav when viewport increases
 $(window).resize(function() {
 	if(window.innerWidth > 768) {
 		$("#nav").removeAttr("style");
 	}
 });
+
+//remove js class from nav for larger screens
+$(document).ready(function() {
+	if(window.innerWidth > 768) {
+		$( "#nav" ).removeClass( "js" );
+	}
+}); 
